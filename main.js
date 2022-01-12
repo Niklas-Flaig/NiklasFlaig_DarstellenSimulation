04e9900a56335d36f1c948b171b0158511c69538
Niklas-Flaig <niklasjensflaig@gmail.com>
2022-01-12 22:46:12 +0100

    added a new function to determine the wished Touchpoint of two circles

--- main.js
+++ main.js
@@ -334,11 +334,7 @@ function atheisticStateV2(params) {
 
     const distance = Math.sqrt(Math.pow(thisElement.yPos - prevElement.yPos, 2) + Math.pow(thisElement.xPos - prevElement.xPos, 2));
 
-    let rest;
-    // while (distance - (thisElement.radius + prevElement.radius) < padding) {
-      // rest = distance - (thisElement.radius + prevElement.radius);
-      
-    // }
+    
 
   }
   
@@ -348,6 +344,38 @@ function atheisticStateV2(params) {
   });
 }
 
+
+
+function determineWishedTouchPoint(circle1, circle2) {
+  const r1 = circle1.radius;
+  const x1 = circle1.xPos;
+  const y1 = circle1.yPos;
+
+  const r2 = circle2.radius;
+  const x2 = circle2.xPos;
+  const y2 = circle2.yPos;
+  // c is the slope of the line between the circle centers
+  const a = y1 - x1 * ((y2 - y1) / (x2 - x1));
+  const b = ((r1*r1) - (r2*r2) - (x1 * x1) + (x2 * x2) - (y1 * y1) + (y2 * y2)) / (2 * (y2 - y1));
+  const c = (x1 - x2) / (y2 - y1);
+  const d = (y2 - y1) / (x2 - x1);
+
+  // equate straight and resolve to x
+  const x = (b - a) / (d - c);
+
+  // get y by pasting x into one of the straights equations
+  const y = (d * x + (y1 - x1 * d));
+
+  console.log(x1 + ":" + y1);
+  console.log(x2 + ":" + y2);
+  console.log(x + ":" + y);
+  console.log(c);
+
+
+
+  // returns the wishedTouchPoints coords
+  return {x: x, y: y};
+}
 
 
 function map(value, minStart, maxStart, minGoal, maxGoal) {
