aa3f481d5882f449f718a3abd44006c9003ff276
Niklas-Flaig <niklasjensflaig@gmail.com>
2022-01-13 09:35:36 +0100

    AKTUELLER STAND

--- helpers.js
+++ helpers.js
@@ -1,4 +1,3 @@
-// this function determines the point, in wich the line between the circle centers touch and the line between the circles intersection
 function determineWishedTouchPoint(circle1, circle2) {
   const r1 = circle1.radius;
   const x1 = circle1.xPos;
@@ -7,15 +6,35 @@ function determineWishedTouchPoint(circle1, circle2) {
   const r2 = circle2.radius;
   const x2 = circle2.xPos;
   const y2 = circle2.yPos;
+  for (let d = x1; d < x2; d++) {
+    let y = ((r1*r1) - (r2*r2) - (x1 * x1 - x2 * x2) - (y1 * y1 - y2 * y2)) / (2 * (y2 - y1)) + ( (2 * d * (x1 - x2)) / (2 * (y2-y1)));
+    // let y = ((y2 - y1)/(x2 - x1)) * d + y1 - ((y2 - y1)/(x2 - x1)) * x1;
+
+    const radius = 5;
+    let obj = $(`<div></div>`);
+    obj.addClass("country");
+    obj.css({
+      width: 2 * radius,
+      height: 2 * radius,
+      left: d - radius,
+      top: y - radius,
+      "background-color": "black",
+      "z-index": 2
+    });
+    stage.append(obj);
+  }
 
   // c is the slope of the line between the circle centers
-  const c = (y2 - y1) / (x2 - x1);
+  const a = y1 - x1 * ((y2 - y1) / (x2 - x1));
+  const b = ((r1*r1) - (r2*r2) - (x1 * x1) + (x2 * x2) - (y1 * y1) + (y2 * y2)) / (2 * (y2 - y1));
+  const c = (x1 - x2) / (y2 - y1);
+  const d = (y2 - y1) / (x2 - x1);
 
   // equate straight and resolve to x
-  const x = ( (r1*r1 - r2*r2) + (x1*x1 - x2*x2) + (y2*y2 - y1*y1) - 2 * ((y2 - y1) * (y1 - x1 * c)))  /  (2 * ((y2 - y1) * c + x2 - x1));
+  const x = (b - a) / (d - c);
 
   // get y by pasting x into one of the straights equations
-  const y = (c * x + (y1 - x1 * c));
+  const y = (d * x + (y1 - x1 * d));
 
   console.log(x1 + ":" + y1);
   console.log(x2 + ":" + y2);
@@ -27,3 +46,61 @@ function determineWishedTouchPoint(circle1, circle2) {
   // returns the wishedTouchPoints coords
   return {x: x, y: y};
 }
+
+
+
+
+
+function determineWishedTouchPoint(circle1, circle2) {
+  const r1 = circle1.radius;
+  const x1 = circle1.xPos;
+  const y1 = circle1.yPos;
+
+  const r2 = circle2.radius;
+  const x2 = circle2.xPos;
+  const y2 = circle2.yPos;
+
+  if (x2 > x1) {
+    for (let d = x1; d < x2; d++) {
+      let y = ((r1*r1) - (r2*r2) + 2 * d * (x1 - x2) - (x1 * x1 - x2 * x2) - (y1 * y1 - y2 * y2)) / (2 * (y2 - y1));
+      // let y = ((y2 - y1)/(x2 - x1)) * d + y1 - ((y2 - y1)/(x2 - x1)) * x1;
+
+      r1 = e
+      r2 = g
+      ((d - b)/(c - a)) * x + b - ((d - b)/(c - a)) * a = ((e*e) - (g*g) + 2 * x * (a - c) - (a * a - c * c) - (b * b - d * d)) / (2 * (d - b))
+
+
+      const radius = 5;
+      let obj = $(`<div></div>`);
+      obj.addClass("country");
+      obj.css({
+        width: 2 * radius,
+        height: 2 * radius,
+        left: d - radius,
+        top: y - radius,
+        "background-color": "black",
+        "z-index": 2
+      });
+      stage.append(obj);
+    }
+  } else {
+    for (let d = x2; d < x1; d--) {
+      let y = ((r1*r1) - (r2*r2) + 2 * d * (x1 - x2) + (x1 * x1 - x2 * x2) - (y1 * y1 - y2 * y2)) / (2 * (y2 - y1));
+
+      const radius = 5;
+      let obj = $(`<div></div>`);
+      obj.addClass("country");
+      obj.css({
+        width: 2 * radius,
+        height: 2 * radius,
+        left: d - radius,
+        top: y - radius,
+        "background-color": "black",
+        "z-index": 2, 
+      });
+      stage.append(obj);
+    }
+  }
+  
+}
+
--- main.js
+++ main.js
@@ -16,9 +16,9 @@ const minSuicideRate = getMinValue("suicideRate");
 
 
 $(function () {
-  atheisticStateV2();
+  // atheisticStateV2();
   // drawRainState(true);
-  // drawMapState();
+  drawMapState();
 });
 // window.addEventListener('resize', drawRainState);
 
@@ -287,7 +287,7 @@ function atheisticStateV2(params) {
     elementLeft.area = map(elementLeft.partialPopulation, 0, maxPopulation, 0, maxArea);
     elementLeft.radius = Math.sqrt(elementLeft.area / Math.PI);
     elementLeft.yPos = map(country.happynesScore, minHappynesScore, maxHappynesScore, stage.innerHeight() - 50, 50);
-    elementLeft.xPos = stage.innerWidth() * 0.7;
+    elementLeft.xPos = stage.innerWidth() * 0.3;
     
     elementLeft.css({
       width: 2 * elementLeft.radius,
@@ -332,18 +332,166 @@ function atheisticStateV2(params) {
     let thisElement = lefts[a]; 
     let prevElement = lefts[a - 1];
 
+    // (when they are stacked onto each other move one initial pixel)
+    if (thisElement.xPos === prevElement.xPos) thisElement.xPos += moveDir;
+
+
     const distance = Math.sqrt(Math.pow(thisElement.yPos - prevElement.yPos, 2) + Math.pow(thisElement.xPos - prevElement.xPos, 2));
 
-    
+    console.log(":  " + distance);
+
+    if (distance - padding < thisElement.radius + prevElement.radius) {
+
+
+      // determine a new TouchPoint
+      let touchPoint = determineWishedTouchPoint(thisElement, prevElement);
+      
+      // determine the x coord of thisElement (at the touchPoints height)
+      let thisElementsX = thisElement.xPos + Math.sqrt(thisElement.radius + Math.pow(touchPoint.y - thisElement.yPos, 2));
+
+      console.log(thisElementsX);
+      // let this element move the right amount to touch the touchpoint
+      thisElement.xPos += thisElement.xPos - thisElementsX
+      // update the css
+      thisElement.css({
+        left: thisElement.xPos
+      });
+      
+
+      // do the same for the prevElement
+      // determine the x coord of thisElement (at the touchPoints height)
+      let prevElementsX = prevElement.xPos + Math.sqrt(prevElement.radius - Math.pow(touchPoint.y - prevElement.yPos, 2));
+
+      // let this element move the right amount to touch the touchpoint
+      prevElement.xPos += prevElement.xPos - prevElementsX;
+      prevElement.css({
+        left: prevElement.xPos
+      });
+
+      // then go back to the prev element and check against their prev element
+      a -= 2;
+
+    }
 
   }
   
   
+  lefts.forEach(element => {
+    stage.append(element);
+  });
   rights.forEach(element => {
     stage.append(element);
   });
 }
 
+function drawPoint(x, y) {
+  console.log("jjjjjj:" + x + ";  "+ y);
+  let elem = $("<div></div>");
+  elem.addClass("country");
+  elem.css({
+    width: 4,
+    height: 4,
+    left: x - 2,
+    top: y - 2,
+    "background-color": "green",
+    "z-index": 5, 
+  });
+  // stage.append(elem);
+}
+
+let circ1 = {
+  radius: 50,
+  xPos: 400,
+  yPos: 470,
+};
+let circ2 = {
+  radius: 50,
+  xPos: 440,
+  yPos: 540,
+};
+
+let res = determineWishedTouchPoint(circ1, circ2);
+let point = {
+  radius: 5,
+  xPos: res.x,
+  yPos: res.y
+};
+
+
+let circ1elem = $(`<div></div>`);
+let circ2elem = $(`<div></div>`);
+let pointPoint = $(`<div></div>`);
+
+circ1elem.addClass("country");
+circ2elem.addClass("country");
+pointPoint.addClass("country");
+
+circ1elem.css({
+  width: 2 * circ1.radius,
+  height: 2 * circ1.radius,
+  left: circ1.xPos - circ1.radius,
+  top: circ1.yPos - circ1.radius,
+  "background-color": "red",
+  "z-index": 1
+});
+
+circ2elem.css({
+  width: 2 * circ2.radius,
+  height: 2 * circ2.radius,
+  left: circ2.xPos - circ2.radius,
+  top: circ2.yPos - circ2.radius,
+  "background-color": "blue",
+  "z-index": 1
+});
+
+// determine a new TouchPoint
+let touchPoint = determineWishedTouchPoint(circ1, circ2);
+      
+// determine the x coord of circ1 (at the touchPoints height)
+let circ1sX = circ1.xPos + Math.sqrt(circ1.radius + Math.pow(touchPoint.y - circ1.yPos, 2));
+console.log("hallo" + circ1sX);
+// if this element is left from the touchpoint we have to use x2 (the right value for y)
+if (circ1.xPos < touchPoint.x) circ1sX -= circ1.xPos - circ1sX;
+
+
+
+console.log(circ1sX);
+// let this element move the right amount to touch the touchpoint
+circ1.xPos += circ1.xPos - circ1sX;
+drawPoint(circ1sX,touchPoint.y);
+// circ1.xPos = circ1.xPos - (circ1sX - touchPoint.x);
+
+// update the css
+circ1elem.css({
+  left: circ1.xPos
+});
+
+
+// do the same for the circ2
+// determine the x coord of thisElement (at the touchPoints height)
+let circ2sX = circ2.xPos + Math.sqrt(circ2.radius + Math.pow(touchPoint.y - circ2.yPos, 2));
+
+if (circ2.xPos < touchPoint.x) circ2sX -= circ2.xPos - circ2sX;
+
+
+// let this element move the right amount to touch the touchpoint
+circ2.xPos += circ2.xPos - circ2sX;
+circ2elem.css({
+  left: circ2.xPos
+});
+
+pointPoint.css({
+  width: 2 * point.radius,
+  height: 2 * point.radius,
+  left: point.xPos - point.radius,
+  top: point.yPos - point.radius,
+  "background-color": "black",
+  "z-index": 3, 
+});
+
+// stage.append(circ1elem);
+// stage.append(circ2elem);
+// stage.append(pointPoint);
 
 
 function determineWishedTouchPoint(circle1, circle2) {
@@ -366,10 +514,10 @@ function determineWishedTouchPoint(circle1, circle2) {
   // get y by pasting x into one of the straights equations
   const y = (d * x + (y1 - x1 * d));
 
-  console.log(x1 + ":" + y1);
-  console.log(x2 + ":" + y2);
-  console.log(x + ":" + y);
-  console.log(c);
+  // console.log(x1 + ":" + y1);
+  // console.log(x2 + ":" + y2);
+  // console.log(x + ":" + y);
+  // console.log(c);
 
 
 
